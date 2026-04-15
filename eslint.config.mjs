fa88ab960f3cad90554fe7fd321df9a198959502
Josu-A <aginagajosu@gmail.com>
2026-04-15 09:03:15 +0200

    monorepo ingurunea konfiguratu

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import perfectionist from 'eslint-plugin-perfectionist';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig(
+    {
+        ignores: [
+            '**/dist/**'
+        ]
+    },
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    perfectionist.configs['recommended-natural'],
+    {
+        rules: {
+            '@typescript-eslint/no-unused-vars': ['error', {
+                argsIgnorePattern: '^_'
+            }]
+        }
+    },
+    {
+        files: ['zerbitzaria/**/*.ts', 'mcp/**/*.ts'],
+        languageOptions: {
+            globals: {
+                ...globals.node
+            }
+        }
+    },
+    {
+        files: ['bezeroa/**/*.{ts,tsx}'],
+        languageOptions: {
+            globals: {
+                ...globals.browser
+            }
+        }
+    }
+);
