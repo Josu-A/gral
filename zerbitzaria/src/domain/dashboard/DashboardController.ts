80912c698e6c6f79ef9a754bccdf56a6a448a656
Josu-A <aginagajosu@gmail.com>
2026-04-23 17:56:11 +0200

    falta zen formatsuccess gehitu

--- zerbitzaria/src/domain/dashboard/DashboardController.ts
+++ zerbitzaria/src/domain/dashboard/DashboardController.ts
@@ -2,6 +2,7 @@ import type { AuthenticatedRequest } from "@common/types";
 import type { NextFunction, Response } from "express";
 
 import HttpStatusCode from "@common/constants/HttpStatusCodes";
+import { formatSuccess } from "@common/utils/responses";
 import DashboardService from "@domain/dashboard/DashboardService";
 import { reqAuthenticated } from "@routers/middleware/authentication";
 
@@ -11,7 +12,7 @@ async function getDashboard(
     _: NextFunction
 ): Promise<void> {
     const data = await DashboardService.getDashboard(req.user.id);
-    res.status(HttpStatusCode.OK).json(data);
+    res.status(HttpStatusCode.OK).json(formatSuccess(data));
 }
 
 export default {
