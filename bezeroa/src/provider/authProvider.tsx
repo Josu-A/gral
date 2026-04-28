6282997908a01aa188c68d271a2dc3008ad98bfb
Josu-A <aginagajosu@gmail.com>
2026-04-28 15:44:16 +0200

    settoken memoizatu

--- bezeroa/src/provider/authProvider.tsx
+++ bezeroa/src/provider/authProvider.tsx
@@ -2,14 +2,15 @@ import {
     createContext,
     type JSX,
     type ReactNode,
+    useCallback,
     useEffect,
     useMemo,
-    useState
-} from 'react';
+    useState,
+} from "react";
 
-import type { AuthContextType } from '@/common/types';
+import type { AuthContextType } from "@/common/types";
 
-import apiClient, { getAccessToken, setAccessToken } from '@/common/apiClient';
+import apiClient, { getAccessToken, setAccessToken } from "@/common/apiClient";
 
 interface AuthProviderProps {
     children: ReactNode;
@@ -21,41 +22,40 @@ function AuthProvider({ children }: AuthProviderProps): JSX.Element {
     const [token, setToken_] = useState<null | string>(getAccessToken());
     const [isLoading, setIsLoading] = useState(true);
 
-    const setToken = (newToken: null | string) => {
+    const setToken = useCallback((newToken: null | string) => {
         setAccessToken(newToken);
         setToken_(newToken);
-    };
+    }, []);
 
     useEffect(() => {
         const initializeAuth = async () => {
             try {
                 const currentAccessToken = getAccessToken();
                 if (!currentAccessToken) {
-                    const response = await apiClient.post<{ data: { accessToken: string } }>(
-                        '/auth/refresh',
-                        {},
-                        { withCredentials: true }
-                    )
+                    const response = await apiClient.post<{
+                        data: { accessToken: string };
+                    }>("/auth/refresh", {}, { withCredentials: true });
                     const newAccessToken = response.data.data.accessToken;
                     setToken(newAccessToken);
                 }
-            }
-            catch {
+            } catch {
                 setToken(null);
-                console.error('Error refreshing access token:');
-            }
-            finally {
+                console.error("Error refreshing access token:");
+            } finally {
                 setIsLoading(false);
             }
         };
         initializeAuth();
-    }, []);
+    }, [setToken]);
 
-    const contextValue = useMemo(() => ({
-        isLoading,
-        setToken,
-        token
-    }), [isLoading, token]);
+    const contextValue = useMemo(
+        () => ({
+            isLoading,
+            setToken,
+            token,
+        }),
+        [isLoading, setToken, token],
+    );
 
     return (
         <AuthContext.Provider value={contextValue}>
