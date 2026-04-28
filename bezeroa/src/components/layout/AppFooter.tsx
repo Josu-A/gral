59de70bb2c6f3253aed975261830fafb7e1c3905
Josu-A <aginagajosu@gmail.com>
2026-04-28 18:30:33 +0200

    footer sinplea gehitu

---
+++ bezeroa/src/components/layout/AppFooter.tsx
@@ -0,0 +1,11 @@
+import { type JSX } from "react";
+
+function AppFooter(): JSX.Element {
+    return (
+        <footer className="bg-gray-800 px-6 py-4 text-white">
+            <p className="text-center">Josu Aguinaga (2026)</p>
+        </footer>
+    );
+}
+
+export default AppFooter;
