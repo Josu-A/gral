d91e6d3cfa600d900362f07e0af82d35d585c369
Josu-A <aginagajosu@gmail.com>
2026-04-29 11:01:54 +0200

    logout-ak ez du behar access lokenik, refresh tokena nahikoa da

--- zerbitzaria/src/routers/auth.router.ts
+++ zerbitzaria/src/routers/auth.router.ts
@@ -1,18 +1,27 @@
-import Paths from '@common/constants/Paths';
-import AuthController from '@domain/auth/AuthController';
-import { authLimiter, restorePasswordLimiter } from '@routers/middleware/rateLimiters';
-import { Router } from 'express';
-
-import { authenticate } from './middleware/authentication';
+import Paths from "@common/constants/Paths";
+import AuthController from "@domain/auth/AuthController";
+import {
+    authLimiter,
+    restorePasswordLimiter,
+} from "@routers/middleware/rateLimiters";
+import { Router } from "express";
 
 const authRouter = Router();
 
 authRouter.post(Paths.Auth.Register, authLimiter, AuthController.register);
 authRouter.post(Paths.Auth.Verify, authLimiter, AuthController.verify);
 authRouter.post(Paths.Auth.Login, authLimiter, AuthController.login);
-authRouter.post(Paths.Auth.Logout, authenticate, AuthController.logout);
+authRouter.post(Paths.Auth.Logout, AuthController.logout);
 authRouter.post(Paths.Auth.Refresh, AuthController.refreshToken);
-authRouter.post(Paths.Auth.RequestPasswordRestore, restorePasswordLimiter, AuthController.requestPasswordRestore);
-authRouter.post(Paths.Auth.RestorePassword, restorePasswordLimiter, AuthController.restorePassword);
+authRouter.post(
+    Paths.Auth.RequestPasswordRestore,
+    restorePasswordLimiter,
+    AuthController.requestPasswordRestore,
+);
+authRouter.post(
+    Paths.Auth.RestorePassword,
+    restorePasswordLimiter,
+    AuthController.restorePassword,
+);
 
 export default authRouter;
