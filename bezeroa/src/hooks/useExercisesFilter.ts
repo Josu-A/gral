407958bc7133702fa6bb232ea88e640ca6340e6a
Josu-A <aginagajosu@gmail.com>
2026-04-30 19:49:36 +0200

    FilterMODE -> FilterMode

--- bezeroa/src/hooks/useExercisesFilter.ts
+++ bezeroa/src/hooks/useExercisesFilter.ts
@@ -5,21 +5,21 @@ import type { Egoera, Zailtasuna } from "@/common/types/entities";
 interface ExercisesFilterApi {
     difficulties: Zailtasuna[];
     programmingLanguageIds: number[];
-    programmingLanguageMode: FilterMODE;
+    programmingLanguageMode: FilterMode;
     setDifficulties: (difficulties: Zailtasuna[]) => void;
     setProgrammingLanguageIds: (ids: number[]) => void;
-    setProgrammingLanguageMode: (mode: FilterMODE) => void;
+    setProgrammingLanguageMode: (mode: FilterMode) => void;
     setStatuses: (statuses: Egoera[]) => void;
     setTagCategoryIds: (ids: number[]) => void;
-    setTagCategoryMode: (mode: FilterMODE) => void;
+    setTagCategoryMode: (mode: FilterMode) => void;
     setTagIds: (tagIds: number[]) => void;
-    setTagMode: (mode: FilterMODE) => void;
+    setTagMode: (mode: FilterMode) => void;
     setTitle: (title: string) => void;
     statuses: Egoera[];
     tagCategoryIds: number[];
-    tagCategoryMode: FilterMODE;
+    tagCategoryMode: FilterMode;
     tagIds: number[];
-    tagMode: FilterMODE;
+    tagMode: FilterMode;
     title: string;
     toggleDifficulty: (value: Zailtasuna) => void;
     toggleProgrammingLanguageId: (id: number) => void;
@@ -28,7 +28,7 @@ interface ExercisesFilterApi {
     toggleTagId: (id: number) => void;
 }
 
-type FilterMODE = "AND" | "OR";
+type FilterMode = "AND" | "OR";
 
 function toggleArrayItem<T>(array: T[], item: T): T[] {
     return array.includes(item)
@@ -42,12 +42,12 @@ function useExercisesFilter(): ExercisesFilterApi {
         number[]
     >([]);
     const [programmingLanguageMode, setProgrammingLanguageMode] =
-        useState<FilterMODE>("OR");
+        useState<FilterMode>("OR");
     const [statuses, setStatuses] = useState<Egoera[]>([]);
     const [tagCategoryIds, setTagCategoryIds] = useState<number[]>([]);
-    const [tagCategoryMode, setTagCategoryMode] = useState<FilterMODE>("OR");
+    const [tagCategoryMode, setTagCategoryMode] = useState<FilterMode>("OR");
     const [tagIds, setTagIds] = useState<number[]>([]);
-    const [tagMode, setTagMode] = useState<FilterMODE>("OR");
+    const [tagMode, setTagMode] = useState<FilterMode>("OR");
     const [title, setTitle] = useState<string>("");
 
     const toggleDifficulty = useCallback((value: Zailtasuna) => {
@@ -94,4 +94,4 @@ function useExercisesFilter(): ExercisesFilterApi {
 }
 
 export { useExercisesFilter };
-export type { ExercisesFilterApi };
+export type { ExercisesFilterApi, FilterMode };
