5a945a987d61b76c6f5a3eff704fd6374357fc68
Josu-A <aginagajosu@gmail.com>
2026-04-30 19:44:40 +0200

    ariketak agertzeko taula sortu

---
+++ bezeroa/src/components/exercises/ExercisesTable.tsx
@@ -0,0 +1,58 @@
+import type { JSX } from "react";
+
+import type { Egoera, Zailtasuna } from "@/common/types/entities";
+
+interface ExerciseRow {
+    ariketa_id: number;
+    egoera: Egoera;
+    izenburua: string;
+    zailtasun_maila: Zailtasuna;
+}
+
+interface ExercisesTableProps {
+    exercises: ExerciseRow[];
+    isLoading: boolean;
+}
+
+function ExercisesTable({
+    exercises,
+    isLoading,
+}: ExercisesTableProps): JSX.Element {
+    if (isLoading) {
+        return <p>Kargatzen...</p>;
+    }
+    if (exercises.length === 0) {
+        return <p>Ez dago ariketarik</p>;
+    }
+    return (
+        <table className="w-full table-auto border-collapse [&_td]:px-4 [&_td]:py-2 [&_td]:whitespace-nowrap [&_th]:px-4 [&_th]:py-2 [&_th]:whitespace-nowrap">
+            <colgroup>
+                <col />
+                <col className="w-1" />
+                <col className="w-1" />
+            </colgroup>
+            <thead>
+                <tr className="border-b-2 border-slate-300 [&>th]:text-left">
+                    <th>Izenburua</th>
+                    <th>Zailtasun maila</th>
+                    <th>Egoera</th>
+                </tr>
+            </thead>
+            <tbody>
+                {exercises.map((exercise) => (
+                    <tr
+                        className="hover:bg-slate-100"
+                        key={exercise.ariketa_id}
+                    >
+                        <td>{exercise.izenburua}</td>
+                        <td>{exercise.zailtasun_maila}</td>
+                        <td>{exercise.egoera}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+}
+
+export { ExercisesTable };
+export type { ExerciseRow };
