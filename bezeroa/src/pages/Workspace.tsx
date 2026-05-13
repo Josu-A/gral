13997871d92c67e02e43b76e811216f727c40ef0
Josu-A <aginagajosu@gmail.com>
2026-05-13 04:22:46 +0200

    saiakerak notaren arabera pintatu

--- bezeroa/src/pages/Workspace.tsx
+++ bezeroa/src/pages/Workspace.tsx
@@ -213,10 +213,12 @@ function supportedLanguageToMonaco(language: string): string {
     return mapping[language] ?? "plaintext";
 }
 
-function viewingAttemptClasses(isViewing: boolean): string {
+function viewingAttemptClasses(isViewing: boolean, score: number): string {
     return clsx(
-        "flex w-full cursor-pointer items-center justify-between rounded-md bg-slate-100 px-3 py-2 hover:bg-slate-300 focus:outline-none focus-visible:ring-1 focus-visible:ring-amber-500 focus-visible:ring-offset-2",
+        "flex w-full cursor-pointer items-center justify-between rounded-md px-3 py-2 hover:bg-slate-300 focus:outline-none focus-visible:ring-1 focus-visible:ring-amber-500 focus-visible:ring-offset-2",
         isViewing && "border-2 border-amber-500",
+        score >= 5 && "bg-emerald-200 text-green-900",
+        score < 5 && "bg-rose-200 text-red-900",
     );
 }
 
@@ -853,6 +855,7 @@ function Workspace(): JSX.Element {
                                                     className={viewingAttemptClasses(
                                                         viewedAttemptId ===
                                                             attempt.saiakera_id,
+                                                        attempt.nota,
                                                     )}
                                                     onClick={() =>
                                                         viewPreviousAttempt(
@@ -861,12 +864,12 @@ function Workspace(): JSX.Element {
                                                     }
                                                     type="button"
                                                 >
-                                                    <span className="text-sm text-gray-800">
+                                                    <span className="text-sm">
                                                         {new Date(
                                                             attempt.denbora_zigilua,
                                                         ).toLocaleString()}
                                                     </span>
-                                                    <span className="text-sm font-medium text-gray-900">
+                                                    <span className="text-sm font-medium">
                                                         {attempt.nota.toFixed(
                                                             2,
                                                         )}
