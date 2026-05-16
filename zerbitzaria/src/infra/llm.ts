f8a305961b4d2bd67adeead50c34b534e85ed67e
Josu-A <aginagajosu@gmail.com>
2026-05-16 04:33:34 +0200

    latxa apiari deia gehitu

--- zerbitzaria/src/infra/llm.ts
+++ zerbitzaria/src/infra/llm.ts
@@ -1,3 +1,4 @@
+import { environment } from "@common/constants/env";
 import logger from "@common/constants/logger";
 import {
     type LlmMessage,
@@ -6,6 +7,7 @@ import {
 } from "@domain/chats/local/types/schemas";
 import fs from "node:fs";
 import path from "node:path";
+import OpenAI from "openai";
 
 interface ContextOptions {
     lastMessagesToKeep?: number;
@@ -21,11 +23,20 @@ interface LlmResponse {
 }
 
 const MAX_MESSAGE_CHAIN_LENGTH = 20;
+const MAX_COMPLETION_TOKENS = 2048;
+const TEMPERATURE = 0.4;
+const MAX_RETRIES = 3;
 
 const systemPromptTemplate = fs.readFileSync(
     path.join(import.meta.dirname, "prompts", "systemPrompt.md"),
     "utf8",
 );
+
+const client = new OpenAI({
+    apiKey: environment.API_KEY_LATXA,
+    baseURL: environment.API_URL_LATXA,
+    maxRetries: MAX_RETRIES,
+});
 
 function createSystemPrompt(data: SystemPromptData) {
     return renderTemplate(systemPromptTemplate, {
@@ -90,15 +101,38 @@ function renderTemplate(
 async function sendMessage(
     messages: LlmMessage[],
 ): Promise<LlmResponse | null> {
-    // TODO
-    logger.info("LLM APIari mezua bidaltzen", { messages });
-    return await new Promise((resolve) =>
-        resolve({
-            content: "Gezurrezko LLMaren erantzuna",
-            model: "default",
-            usage: { completionTokens: 0, promptTokens: 0 },
-        }),
-    );
+    logger.info("LLM APIari mezua bidaltzen", {
+        messages: JSON.stringify(messages),
+    });
+    try {
+        const completion = await client.chat.completions.create({
+            max_tokens: MAX_COMPLETION_TOKENS,
+            messages,
+            model: environment.API_MODEL_LATXA,
+            temperature: TEMPERATURE,
+        });
+
+        const choice = completion.choices[0];
+
+        if (!choice?.message.content) {
+            logger.warn("LLM APIak ez du edukirik itzuli", { completion });
+            return null;
+        }
+
+        return {
+            content: choice.message.content,
+            model: completion.model,
+            usage: {
+                completionTokens: completion.usage?.completion_tokens ?? 0,
+                promptTokens: completion.usage?.prompt_tokens ?? 0,
+            },
+        };
+    } catch (error) {
+        logger.error("Errorea LLM APIarekin komunikatzean:", {
+            error: error instanceof Error ? error.message : String(error),
+        });
+        return null;
+    }
 }
 
 export default {
